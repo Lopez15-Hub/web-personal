c579ac33a47d8e1170697b071d0744c86f5df91f
Lopez15-Hub <65269387+Lopez15-Hub@users.noreply.github.com>
2021-04-09 17:11:50 -0300

    asdasd

---
+++ frontend/src/components/views/contact.js
@@ -0,0 +1,17 @@
+import Form from '../formContact';
+import Nav from '../nav';
+import BodyInicio from '../bodyInicio';
+import Footer from "../footer";
+function Contact (){
+    return(
+        
+     <div>
+    <Nav />
+        <Form/>
+        <Footer />
+     </div>
+    
+     
+    );
+}
+export default Contact;
